7f7fc651688c4144e58f2e97dd473944f32d0e76
周泽勇 <zzycami@gmail.com>
2015-07-29 15:00:22 +0800

    Initial Commit

---
+++ SidePanelController/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SidePanelController
+//
+//  Created by 周泽勇 on 15/7/29.
+//  Copyright (c) 2015年 周泽勇. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
